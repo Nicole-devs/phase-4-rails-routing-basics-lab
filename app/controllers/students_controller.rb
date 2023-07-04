b7082db8c0f1bcd530610a950793b18db2d3e1c9
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-07-04 14:52:49 +0300

    highest_grade action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,10 +3,15 @@ class StudentsController < ApplicationController
         students = Students.all
         render json: students
     end
-    
+
     def grades
         students = Student.order(grade: :desc)
         render json: students
+    end
+    
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
     end
         
 end
